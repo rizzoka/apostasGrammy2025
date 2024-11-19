62e08317e5d50640cb4e5580eb020b9cdacdcf3b
RizzoKa <kauarizzo06@gmail.com>
2024-11-19 17:46:15 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function albumano(){
+    alert("New Blue Sun - André 3000; \nCowboy Carter - Beyoncé; \nShort n' Sweet - Sabrina Carpenter; \nBrat - Charli XCX; \nDjesse Vol. 4 - Jacob Collier; \nHit Me Hard and Soft - Billie Eilish; \nThe Rise and Fall of a Midwest Princess - Chappell Roan; \nThe Tortured Poets Department - Taylor Swift.")
+}
+function musicaano(){
+    alert("A Bar Song (Tipsy) - Shaboozey; \nBirds of a Feather - Billie Eilish; \nDie with a Smile - Lady Gaga e Bruno Mars; \nFortnight - Taylor Swift e Post Malone; \nGood Luck, Babe! - Chappell Roan;\nNot Like Us - Kendrick Lamar; \nPlease Please Please - Sabrina Carpenter; \nTexas Hold'Em - Beyoncé.")
+}
+function artistaano(){
+    alert("Benson Boone; \nSabrina Carpenter; \nDoechii; \nKhruangbin; \nRaye; \nChappell Roan; \nShaboozey; \nTeddy Swims")
+}
+function gravacaoano(){
+    alert("Now and Then - The Beatles; \nTexas Hold'Em - Beyoncé;\nEspresso - Sabrina Carpenter;\n360 - Charli XCX;\nBirds of a Feather - Billie Eilish;\nNot Like Us - Kendrick Lamar;\nGood Luck, Babe! - Chappell Roan;\nFortnight - Taylor Swift.")
+}
+function melhorpopduo(){
+    alert("Us - Gracie Abrams feat Taylor Swift;\nLevii's Jeans - Beyoncé feat Post Malone;\nGuess - Charlie XCX feat Billie Eilish;\nThe Boy is Mine - Ariana Grande feat Brandy & Monica;\nDie with a Smile - Bruno Mars feat Lady Gaga.")
+}
